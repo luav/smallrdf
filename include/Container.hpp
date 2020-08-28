2692717b99449eedbe414a186f6ebc51a633244a
Artem <luardev@gmail.com>
2020-08-28 23:21:06 +0200

    Container interface refined

--- include/Container.hpp
+++ include/Container.hpp
@@ -5,101 +5,104 @@
 #ifndef CONTAINER_HPP_
 #define CONTAINER_HPP_
 
+//#include <stdexcept>
+
 namespace smallrdf {
+
+//using std::out_of_range;
 
 template<typename T>
 class Container {
- public:
-  virtual int length() const=0;
-  virtual T add(const T& item)=0;
-  virtual T get(int index) const=0;
-  virtual T operator[](int index) const final  { return get(index); }
+public:
+	virtual unsigned length() const noexcept=0;
+	virtual T& add(const T& item) noexcept=0;
+
+    //! \brief Fetch node by index
+    //!
+    //! \param index int  - index, a negative value is treated as index from the end
+    //! \return ListNode<T>*  - resulting node
+	virtual T& get(int index)=0;
+	virtual const T& get(int index) const=0;
+	virtual T& operator[](int index) final  { return get(index); }
+	virtual const T& operator[](int index) const final  { return get(index); }
 };
 
 // List --------------------------------------------------------------------
 template<typename T>
-class ListNode {
-public:
-  T item;
-  ListNode<T>* next;
+struct ListNode {
+	ListNode(const T& val): item(val), next(nullptr)  {}
+
+	T item;
+	ListNode<T>* next;
 };
 
 template<typename T>
 class List: Container<T> {
- public:
-  int length() const override  { return _length; }
+public:
+	unsigned length() const noexcept override  { return _length; }
 
-  List();
-  ~List();
+	List() noexcept;
+	~List();
 
-  T add(const T& item) override;
-  T get(int index) const override;
-
+	T& add(const T& item) noexcept override;
+	T& get(int index) override;
+	const T& get(int index) const override;
 private:
-  int _length;
- protected:
-  ListNode<T>* _root;
-  ListNode<T>* _last;
+	unsigned _length;
+protected:
+	ListNode<T>* _root;
+	ListNode<T>* _last;
 
-  ListNode<T>* node(int index) const;
+    //! \brief Fetch node by index
+    //!
+    //! \param index int  - index, a negative value is treated as index from the end
+    //! \return ListNode<T>*  - resulting node
+	ListNode<T>* node(int index) const noexcept;
 };
 
-template<typename T> List<T>::List()
-    : _length(0),
-      _root(0),
-      _last(0) {
+template<typename T> List<T>::List() noexcept
+: _length(0), _root(nullptr), _last(nullptr) {
 }
 
 template<typename T> List<T>::~List() {
-  while (_root) {
-    ListNode<T>* cur = _root;
-    _root = _root->next;
-    delete cur;
-  }
+	while (_root) {
+	  ListNode<T>* cur = _root;
+	  _root = _root->next;
+	  delete cur;
+	}
 }
 
-template<typename T> ListNode<T>* List<T>::node(int index) const {
-  int pos = 0;
-  ListNode<T>* cur = _root;
+template<typename T> ListNode<T>* List<T>::node(int index) const noexcept {
+	ListNode<T>* cur = _root;
 
-  while (pos < index && cur) {
-    cur = cur->next;
-    pos++;
-  }
-
-  if (pos == index) {
-    return cur;
-  }
-
-  return 0;
+	if(index < 0)
+		index += _length;
+	while (index-- && cur)
+	  cur = cur->next;
+	return cur;
 }
 
-template<typename T> T List<T>::add(const T& item) {
-  ListNode<T>* cur = new ListNode<T>();
+template<typename T> T& List<T>::add(const T& item) noexcept {
+	ListNode<T>* cur = new ListNode<T>(item);
 
-  cur->item = item;
-  cur->next = 0;
+	if (_root) {
+	  _last->next = cur;
+	  _last = cur;
+	}
+	else _root = _last = cur;
+	++_length;
 
-  if (_root) {
-    _last->next = cur;
-    _last = cur;
-  } else {
-    _root = _last = cur;
-  }
-
-  ++_length;
-
-  return item;
+	return *cur;
 }
 
-template<typename T> T List<T>::get(int index) const {
-  ListNode<T>* cur = node(index);
+template<typename T> T& List<T>::get(int index) {
+	ListNode<T>* cur = node(index);
 
-  if (cur) {
-    return cur->item;
-  } else {
-    return 0;
-  }
+//	// Note: the app is crashed if cur = nullptr exception is not handled,
+//	// but this handling affects performance.
+//	if (!cur)
+//		throw out_of_range("The index is out of range");
+	return cur->item;
 }
 
 }  // smallrdf
